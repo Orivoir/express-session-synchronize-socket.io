e18070256631e05dbef4be2daa735c2996233adb
S.Gaborieau <sam.gabor@hotmail.com>
2019-12-24 17:01:49 +0100

    doc block entry point

--- express-session-synchronize-socket.io.js
+++ express-session-synchronize-socket.io.js
@@ -1,13 +1,40 @@
+/**
+ * @author S.Gaborieau <sam.gabor@hotmail.com>
+ *
+ * @package [npm] express-session-synchronize-socket.io
+ * @version 0.1.5 `stable version`
+ *
+ * @git <https://github.com/Orivoir/express-session-synchronize-socket.io>
+ * @npm <https://www.npmjs.com/package/express-session-synchronize-socket.io>
+ */
+
 const
-    // memory store
+    /**
+     * @const {object} session
+     * @description memory store **HTTP** and **TCP/IP** contains **synchronizer methods**
+     */
     session = require('./lib/session')
 
-    // callbacks dist
+    /**
+     * @const {object} middlewares
+     * @method `express`
+     * @method `socketIO`
+     * @description contains **middlewares** `methods` for **router** `express` *and* `socket.io` **module**
+     */
     ,middlewares = require('./lib/middlewares')( session )
 ;
 
+/**
+ * @export {Function}
+ * @param {string|RegExp} pathGetSession path access session HTTP value from `GET` route **HTTP session in micro-service**
+ * @param {boolean} autoSynchro auto synchronize with `HTTP` model between `HTTP` request
+ * @return {object} middlewares
+ * @description **hydrate config** with @params and `return` **middlewares** methods
+ */
 module.exports = function(
+
     pathGetSession = "/get/session"
+
     ,autoSynchro = true
 ) {
 
